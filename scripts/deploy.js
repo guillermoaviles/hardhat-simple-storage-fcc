1e761e3ff029c873e8f25de7f691726dfff8c2f9
Guillermo Aviles <gjaviles7@gmail.com>
2023-08-15 21:29:27 -0400

    sentTxResponse

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,32 @@ async function main() {
         "http://0.0.0.0:8545"
     );
     const wallet = new ethers.Wallet(
-        "0xaeac1fd23966834d51a825fc6c17279595728211b57e9e8875cbaac6fa1201f8",
+        "0x1e049a6503d352bfad84176e83f606eca280de9bf50c769d272e859954b589d1",
         provider
     );
 
     const abi = fs.readFileSync("./contracts_SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./contracts_SimpleStorage_sol_SimpleStorage.bin", "utf8");
 
-    // ContractFactory
-    const contractFactory = new ethers.ContractFactory(abi, binary, wallet); 
+    // Ensure the contract bytecode is properly hex-encoded
+    const bytecode = "0x" + binary.trim(); // Add '0x' prefix
 
-    console.log('Deploying, please wait...');
-    const contract = await contractFactory.deploy();
-    console.log(contract);
+    // Use the correct chainId for the network you're using
+    const chainId = await provider.getNetwork().then(network => network.chainId);
+
+    const tx = {
+        nonce: 5,
+        gasPrice: 20000000000, // Use parseUnits to convert to Wei
+        gasLimit: 6721975,
+        to: null,
+        value: 0,
+        data: bytecode, // Use the properly hex-encoded bytecode here
+        chainId: chainId, // Use the actual chainId of the network
+    };
+    
+    const sentTxResponse = await wallet.sendTransaction(tx);
+    await sentTxResponse.wait(1);
+    console.log('sentTxResponse', sentTxResponse);
 }
 
 main()
