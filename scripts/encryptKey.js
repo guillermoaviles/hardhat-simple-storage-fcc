d9c491c200053124241025eb5ca0fdf3b032efce
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-04 21:59:33 -0400

    reverted encrypt key

--- scripts/encryptKey.js
+++ scripts/encryptKey.js
@@ -4,11 +4,9 @@ require("dotenv").config();
 
 async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-    const encryptedJsonKey = await wallet.encrypt(
-        process.env.PRIVATE_KEY_PASSWORD,
-        process.env.PRIVATE_KEY
-    );
+    const encryptedJsonKey = await wallet.encrypt(process.env.PRIVATE_KEY_PASSWORD);
     console.log('encryptedJsonKey: ', encryptedJsonKey);
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
 }
 
 main()
